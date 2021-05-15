d5981c907d10d848a560242829bc9b35f7508386
Yashas Shah <79524299+coolcoder-js@users.noreply.github.com>
2021-05-15 10:11:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var app_1 = new Vue({
   el: '#app-6',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello!'
   }
 })
 
 var app_2 = new Vue({
   el: '#app-5',
   data: {
-    message: 'Hello Vue.js!'
+    message: 'Hello!'
   },
   methods: {
     reverseMessage: function () {
